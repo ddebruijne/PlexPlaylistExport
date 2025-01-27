3006f01e88063fd0f7248ac704fd5167a09e2234
Danny de Bruijne <dannydb44@gmail.com>
2025-01-27 22:04:22 +0100

    batch export all playlists

---
+++ exportplaylists.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./PlexPlaylistExport.py --host http://plex.jn --token PEfedhc4q8DxF5ySgoU- --playlist "Final Fantasy XIV and XVI" --plex-music-root="/media/Music" --replace-with-dir ".."
+./PlexPlaylistExport.py --host http://plex.jn --token PEfedhc4q8DxF5ySgoU- --playlist "Persona" --plex-music-root="/media/Music" --replace-with-dir ".."
