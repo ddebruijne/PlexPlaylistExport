ab6c0a40169dbd6987d47dc3f524a1cda322eecf
Danny de Bruijne <dannydb44@gmail.com>
2025-01-28 19:48:26 +0100

    add renaming and parsing playlist by array

--- exportplaylists.sh
+++ exportplaylists.sh
@@ -6,15 +6,36 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# Capture the input parameter
 token=$1
+playlistfolder="/run/user/1000/gvfs/smb-share:server=192.168.103.7,share=media/Music/Playlists"
+playlists=( # Maybe replace this with a --list, save to file, then read all playlist for uber automatic export
+    "Persona"
+    "Final Fantasy XIV and XVI"
+    "Songe"
+    "NieR"
+    "prime weeb shit"
+    "Monster Hunter"
+    "The Best OSTs"
+)
 
-./PlexPlaylistExport.py --host http://plex.jn --token $token \
-    --playlist "Final Fantasy XIV and XVI" \
-    --plex-music-root="/media/Music" \
-    --replace-with-dir ".."
+# parse all plists
+for item in "${playlists[@]}"; do
+    echo "Parsing $item"
+    ./PlexPlaylistExport.py --host http://plex.jn --token $token \
+        --playlist "$item" \
+        --plex-music-root="/media/Music" \
+        --replace-with-dir ".."
+done
 
-./PlexPlaylistExport.py --host http://plex.jn --token $token \
-    --playlist "Persona" \
-    --plex-music-root="/media/Music" \
-    --replace-with-dir ".."
+# dumb rename, ascii-ify didnt work very well for me
+old_ext="m3u8"
+new_ext="m3u"
+for file in "out"/*."$old_ext"; do
+    [ -e "$file" ] || continue
+    new_file="${file%.$old_ext}.$new_ext"
+    mv "$file" "$new_file"
+    echo "Renamed: '$file' -> '$new_file'"
+done
+
+echo "Copying playlists to $playlistfolder"
+cp out/* $playlistfolder/
