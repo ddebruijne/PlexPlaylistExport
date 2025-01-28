9ab27da84664bd4403d72396d767210387c5c64e
Danny de Bruijne <dannydb44@gmail.com>
2025-01-28 19:19:46 +0100

    parameterize token input

--- exportplaylists.sh
+++ exportplaylists.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-./PlexPlaylistExport.py --host http://plex.jn --token PEfedhc4q8DxF5ySgoU- --playlist "Final Fantasy XIV and XVI" --plex-music-root="/media/Music" --replace-with-dir ".."
-./PlexPlaylistExport.py --host http://plex.jn --token PEfedhc4q8DxF5ySgoU- --playlist "Persona" --plex-music-root="/media/Music" --replace-with-dir ".."
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    echo "No input provided. please pass in the plex token."
+    exit 1
+fi
+
+# Capture the input parameter
+token=$1
+
+./PlexPlaylistExport.py --host http://plex.jn --token $token \
+    --playlist "Final Fantasy XIV and XVI" \
+    --plex-music-root="/media/Music" \
+    --replace-with-dir ".."
+
+./PlexPlaylistExport.py --host http://plex.jn --token $token \
+    --playlist "Persona" \
+    --plex-music-root="/media/Music" \
+    --replace-with-dir ".."
